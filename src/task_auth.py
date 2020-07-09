721958b9d8e9579a295422164f4f0f8aa104b375
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-07-09 17:45:34 +0100

    Authenticate with OW

--- src/task_auth.py
+++ src/task_auth.py
@@ -1,10 +1,12 @@
 from .celery import app
 from .config import Config
 import montagu
+import orderlyweb_api
 
 
 @app.task
 def auth():
     config = Config()
     monty = montagu.MontaguAPI(config.montagu_url, config.montagu_user, config.montagu_password)
-    return monty.token
+    ow = orderlyweb_api.OrderlyWebAPI('http://localhost:8888', monty.token)
+    return ow.token
