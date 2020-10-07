6d79204dfa73ff1bb4e92855ef99ddf4d58c873b
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-10-07 17:31:48 +0100

    Add timeout to config

--- src/utils/run_reports.py
+++ src/utils/run_reports.py
@@ -25,7 +25,8 @@ def run_reports(wrapper, config, reports, success_callback):
         try:
             key = wrapper.execute(wrapper.ow.run_report,
                                   report.name,
-                                  report.parameters)
+                                  report.parameters,
+                                  report.timeout)
 
             running_reports[key] = report
             logging.info("Running report: {}. Key is {}".format(report.name,
