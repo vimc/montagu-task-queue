f3f0cbcb924b844ae2e8eb5db6d74ff2bdc7f3dd
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-03-21 17:12:54 +0000

    Add permission when create dir

--- scripts/run-docker-worker.sh
+++ scripts/run-docker-worker.sh
@@ -9,7 +9,7 @@ ARCHIVE_DIR=test_archive_files
 LOCAL_ARCHIVE_DIR=$HERE/../$ARCHIVE_DIR
 
 rm $LOCAL_ARCHIVE_DIR -rf
-mkdir $LOCAL_ARCHIVE_DIR
+mkdir -m a+rw $LOCAL_ARCHIVE_DIR
 
 
 docker run --env YOUTRACK_TOKEN=$YOUTRACK_TOKEN \
