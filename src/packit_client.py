0016010378b283f3f3b75c0535bff4bc8d92db03
Emma <EmmaRussellKilburn@gmail.com>
2025-06-12 15:33:12 +0100

    new branch

---
+++ src/packit_client.py
@@ -0,0 +1,13 @@
+import montagu
+
+# TODO: How will we be able to recognised expired token?
+
+class PackitClient:
+    def __init__(self, config):
+        self.config = config
+
+    #def __get()
+    #def __post()
+    #def run_packet()
+    #def poll_running_packet()
+    #def kill_running_packet()
