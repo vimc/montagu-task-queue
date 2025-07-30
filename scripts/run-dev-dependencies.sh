f529a7e7f071c8ae5e3b8911db18b73881dff4d9
Emma <EmmaRussellKilburn@gmail.com>
2025-07-30 17:17:18 +0100

    remove exit code ignore in cleanup too

--- scripts/run-dev-dependencies.sh
+++ scripts/run-dev-dependencies.sh
@@ -7,10 +7,7 @@ set -ex
 function cleanup() {
   docker container stop reverse-proxy
   docker container rm reverse-proxy -v
-  # Same exit code issue for packit stop as packit start....
-  set +e
   hatch env run -- packit stop
-  set -e
   # remove db volume manually rather than --volumes flag to packit, to avoid requiring user confirmation
   docker volume rm montagu_packit_db montagu_orderly_library montagu_outpack_volume montagu_orderly_logs
   docker compose --project-name montagu down -v
